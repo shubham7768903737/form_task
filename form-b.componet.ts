673a627d684df6b72e5f1304f4e7d8154e54a7ef
shubham7768903737 <sparit3737@gmail.com>
2024-08-23 18:01:11 +0530

    Create form-b.componet.ts

---
+++ form-b.componet.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-b',
+  templateUrl: './form-b.component.html',
+  styleUrl: './form-b.component.css'
+})
+export class FormBComponent {
+  public selectVal : string = " ";
+}
